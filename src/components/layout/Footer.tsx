ce99dc9d8a25c2e98dd02277290d93238ef2721e
copyrightsol <copyright.sol@proton.me>
2024-05-13 22:36:45 +0200

    Update Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -7,14 +7,6 @@ export default function Footer() {
     <footer className="bg-gradient-to-br from-blue-800 to-purple-800 rounded-t-2xl shadow-t-3xl">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://gamba.so/"
-            className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
-          >
-            <img src="/logo.svg" className="h-10" alt="Gamba Logo" />
-          </a>
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
             {FOOTER_LINKS.map((link) => (
               <li key={link.href}>
