305791ad548c6ea3bcdb57825270ead1974bb68f
copyrightsol <copyright.sol@proton.me>
2024-05-13 22:44:14 +0200

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -7,7 +7,7 @@ export const PLATFORM_CREATOR_ADDRESS = new PublicKey(
 );
 
 // Platform fees
-export const PLATFORM_CREATOR_FEE = 0.15; // 5%
+export const PLATFORM_CREATOR_FEE = 0.15; // 15%
 export const PLATFORM_JACKPOT_FEE = 0.01; // 1%
 
 // Toggle live toast events - all game events (true = on, false = off)
@@ -21,26 +21,26 @@ export const LIVE_EVENT_TOAST = true;
 
 export const FOOTER_LINKS = [
   {
-    href: "https://github.com/BankkRoll/Gamba-V2-Next.js",
-    title: "👨‍💻 Build your own",
+    href: "http://cprsol.com/",
+    title: "👨‍💻 Watch the Vision",
   },
   {
-    href: "https://explorer.gamba.so/create",
-    title: "🚀 Create Pool",
+    href: "https://t.me/copyrightsolv2",
+    title: "🚀 Join Telegram",
   },
   {
-    href: "https://gamba.so/docs",
-    title: "📖 Gamba Docs",
+    href: "https://copyrightsol.gitbook.io/whitepaper",
+    title: "📖 Copyright Docs",
   },
   {
-    href: "https://discord.com/invite/HSTtFFwR",
+    href: "https://discord.gg/copyrightsol",
     title: "💬 Join Discord",
   },
 ];
 
 export const FOOTER_TWITTER_LINK = {
-  href: "https://twitter.com/bankkroll_eth",
-  title: "© 2024 Template made with ❤️ by Bankk",
+  href: "https://twitter.com/copyrightsol",
+  title: "© 2024 CPRSOL.FUN is owned and operated by COPYRIGHTSOL Holdings Limited (Cyprus). Contact us at contact@cprsol.com.",
 };
 
 /******************************************
@@ -50,28 +50,28 @@ export const FOOTER_TWITTER_LINK = {
  ******************************************/
 
 export const BASE_SEO_CONFIG = {
-  defaultTitle: "Gamba - NEXTjs Demo",
+  defaultTitle: "Copyrightsol.fun",
   description:
     "The gambleFi protocol with end-to-end tools for on-chain degeneracy on Solana.",
   openGraph: {
     type: "website",
     locale: "en_US",
     url: "https://play-gamba.vercel.app/",
-    title: "Gamba - NEXTjs Demo",
+    title: "Copyrightsol.fun",
     description:
       "The gambleFi protocol with end-to-end tools for on-chain degeneracy on Solana.",
     images: [
       {
         url: "https://play-gamba.vercel.app/seo.png",
-        alt: "Gamba - NEXTjs Demo",
+        alt: "Copyrightsol.fun",
       },
     ],
-    site_name: "Gamba - NEXTjs Demo",
+    site_name: "Copyrightsol.fun",
   },
   twitter: {
     cardType: "summary_large_image",
-    site: "https://twitter.com/gambalabs",
-    handle: "@gambalabs",
+    site: "https://twitter.com/copyrightsol",
+    handle: "@copyrightsol",
   },
   additionalMetaTags: [
     {
